0a7ddc8c427ed9dc6ac31553bd2b9e2e401162e8
Krishna Addepalli <coolkrishna31@gmail.com>
2024-03-07 10:09:32 +0100

    Use tangent space for lighting calculations

--- wgpu_main/src/model.rs
+++ wgpu_main/src/model.rs
@@ -11,12 +11,40 @@ pub trait Vertex {
 
 /// `bytemuck::Pod` indicates that our `Vertex` is "Plain Old Data" and can be interpreted as a &[u8].
 /// `bytemuck::Zeroable` indicates that we can use `std::mem::zeroed()`.
+///
+/// # Tangent Space to World Space
+/// When we pull the normal data from our normal texture, all the normals are in what's known as pointing
+/// roughly in the positive z direction. That means that our lighting calculation thinks all the
+/// surfaces of our models are facing roughly in the same direction. This is referred to as `tangent space`.
+/// It turns out that we can use the vertex normal (indicating the direction of the surface), to
+/// transform our normals from `tangent space` into `world space`.
+///
+/// We can create a matrix that represents a coordinate system using three vectors that are perpendicular
+/// (or orthonormal) to each other. We're going to create a matrix that will represent the coordinate
+/// space relative to our vertex normals. We're then going to use that to transform our normal map
+/// data to be in world space.
+///
+/// # The tangent and the bitangent
+///
+/// A tangent represents any vector parallel with a surface (aka. doesn't intersect the surface). The
+/// tangent is always perpendicular to the normal vector. The bitangent is a tangent vector that is
+/// perpendicular to the other tangent vector. Together, the tangent, bitangent and normal represent
+/// the x, y, and z axes of a coordinate system.
+///
+/// Some model formats include the tangent and bitangent (sometimes called the binormal) in the vertex
+/// data, but OJB does not. We'll have to calculate them manually. Luckily, we can derive our tangent
+/// and bitangent from our existing vertex data.
+/// ![Tangent and Bitangent][normal_tangent_bitangent_coordinate_system.png]
+///
+/// Basically, we can use the edges of our triangles and our normal to calculate the tangent and bitangent.
 #[repr(C)]
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct ModelVertex {
     pub position: [f32; 3],
     pub tex_coords: [f32; 2],
     pub normal: [f32; 3],
+    pub tangent: [f32; 3],
+    pub bitangent: [f32; 3],
 }
 
 impl Vertex for ModelVertex {
@@ -53,6 +81,17 @@ impl Vertex for ModelVertex {
                 wgpu::VertexAttribute {
                     offset: mem::size_of::<[f32; 5]>() as wgpu::BufferAddress,
                     shader_location: 2,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                // Tangent and bitangent
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                    shader_location: 3,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 11]>() as wgpu::BufferAddress,
+                    shader_location: 4,
                     format: wgpu::VertexFormat::Float32x3,
                 },
             ],
